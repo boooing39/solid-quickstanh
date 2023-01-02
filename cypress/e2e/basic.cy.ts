172d159c5f16776c0a4b2877d7499f7c2928de15
boooing39 <121826427+boooing39@users.noreply.github.com>
2023-01-02 20:58:49 +0100

    Update basic.cy.ts

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -3,10 +3,6 @@ describe('empty spec', () => {
     cy.visit('/')
   })
 
-  it('display header text', () => {
-    cy.get('h1')
-    .contains('Hello World!', { matchCase: false })
-  })
 
   it('displays Click button', () => {
     cy.get('button')
